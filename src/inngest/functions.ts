13cf633ad1cbb239b020df7d31400170495fcf93
Grzegorz Studziński <grzegorz.studzinski.2007@gmail.com>
2025-12-10 13:09:12 +0100

    gemini

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -63,7 +63,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "An expert coding agent",
       system: PROMPT,
       model: grok({ 
-        model: "gemini-2.5-flash",
+        model: "gemini-1.5-flash",
         defaultParameters: {
           temperature: 0.1,
         },
@@ -196,7 +196,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "A fragment title generator",
       system: FRAGMENT_TITLE_PROMPT,
       model: grok({ 
-        model: "gemini-2.5-flash",
+        model: "gemini-1.5-flash",
       }),
     })
 
@@ -205,7 +205,7 @@ export const codeAgentFunction = inngest.createFunction(
       description: "A response generator",
       system: RESPONSE_PROMPT,
       model: grok({ 
-        model: "gemini-2.5-flash",
+        model: "gemini-1.5-flash",
       }),
     });
 

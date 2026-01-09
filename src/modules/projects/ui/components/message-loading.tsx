07848c742a8e5bc6c9c232d76d559f9134ea1b81
Grzegorz <grzegorz.studzinski.2007@gmail.com>
2026-01-09 21:31:12 +0100

    Revert "Język polski strony"
    This reverts commit b340a488a1f032cb11c7997ed6b2b1119324d230.

--- src/modules/projects/ui/components/message-loading.tsx
+++ src/modules/projects/ui/components/message-loading.tsx
@@ -38,12 +38,12 @@ export const MessageLoading = () => {
       <div className="flex items-center gap-2 pl-2 mb-2">
         <Image
           src="/logo.svg"
-          alt="Przekod"
+          alt="Vibe"
           width={18}
           height={18}
           className="shrink-0"
         />
-        <span className="text-sm font-medium">Przekod</span>
+        <span className="text-sm font-medium">Vibe</span>
       </div>
       <div className="pl-8.5 flex flex-col gap-y-4">
         <ShimmerMessages />
